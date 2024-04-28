67fbffab31cb08d59209cbef12d6ce52e067a279
frederiklange <frederik.lange@hotmail.com>
2024-04-28 11:31:56 +0200

    first simple version of dotfiles

---
+++ install_ubuntu.sh
@@ -0,0 +1,35 @@
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# added homebrew to PATH and to bash shell rcfile
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/frederik-lange/.bashrc
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# install essential built tools
+sudo apt-get install build-essential procps curl file git
+
+# install packages
+brew install gcc
+brew install git
+brew install zsh
+brew install starship
+brew install fzf
+brew install ripgrep
+brew install neovim
+brew install unzip
+brew install tmux # may come as a defaul in some ubuntu version
+brew install stow
+
+# stow
+stow zsh
+stow starship
+stow tmux
+stow nvim
+
+# add zsh to valid login shells
+command -v zsh | sudo tee -a /etc/shells
+
+# use zsh as default shell
+sudo chsh -s $(which zsh) $USER
+
+
---
+++ zsh/.zshrc
@@ -0,0 +1,11 @@
+# use homebrew
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# use startship
+eval "$(starship init zsh)"
+
+# use fzf key bindings and completions
+eval "$(fzf --zsh)"
+
+# aliases
+alias vim="nvim"
